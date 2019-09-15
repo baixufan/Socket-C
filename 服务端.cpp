4d0f27f8d09bd1d32526c8846cbcc682780f935e
baixufan <544206996@qq.com>
2019-09-15 20:18:20 +0800

    第二次修改
    实现同一台主机互通
    IP使用回送地址127.0.0.1

--- 服务端.cpp
+++ 服务端.cpp
@@ -1,87 +1,99 @@
+#include "winsock2.h"
+#pragma comment(lib, "ws2_32.lib")
 #include <iostream>
-#include <windows.h>
-
 using namespace std;
-
-#pragma comment(lib,"ws2_32.lib")//引用静态链接库
-
-int main()
+int main(int argc, char* argv[])
 {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	
-	wVersionRequested=MAKEWORD(2,2);
-	
-	err = WSAStartup( wVersionRequested,&wsaData);
-	
-	if(err!=0)
-	{
-		//tell the user we could nto find a usable
-		//Winsock DLL;
-		return 0;
-	}
-	
-	if(LOBYTE(wsaData.wVersion)!=2 || HIBYTE(wsaData.wVersion)!=2)
-	{
-		WSACleanup();
-		return 0;
-	}
-	
-	//The WinSock DLL is acceptable,Proceed windows下版本的协商
-	///////////////////////////////////////////////////////////
-	SOCKET sockSer;//服务器套接字
-	sockSer = socket(AF_INET,SOCK_STREAM,0);//创建套接字，AF_INET代表IP家族，0是默认的方式创建 有连接是流式 无连接是数据包套接字
-	
-	SOCKADDR_IN addrSer,addrCli;
-	addrSer.sin_family = AF_INET;
-	addrSer.sin_port = htons(5050);
-	addrSer.sin_addr.S_un.S_addr = inet_addr("192.168.3.173");
-	
-	bind(sockSer,(SOCKADDR*)&addrSer,sizeof(SOCKADDR));//绑定套接字 跟服务器相关地址信息进行绑定
-	
-	listen(sockSer,5);//监听套接字 5是队列大小
-	
-	SOCKET sockConn;//给一个相应的套接字
-	int len = sizeof(SOCKADDR);
-	
-	cout<<"Server wait Client Connect........"<<endl;
-	sockConn = accept(sockSer,(SOCKADDR*)&addrCli,&len);//处理新到连接 3个参数分别是服务器的套接字号；客户端地址，相应地址层度的地址以地址形式传递
-	
-	
-	if(sockConn == INVALID_SOCKET)//INVALID_SOCKET非法套接字
-	{  
-		cout<<"Server Accept Client Connect Fail"<<endl;
-		return 0;
-	}
-	else
-	{
-		cout<<"Server Accept Client Connect Success"<<endl;
-		return 0;	
-	}
-	
-	
-	char sendbuf[256];
-	char recvbuf[256];
-	
-	while(1)
-	{
-		cout<<"Ser: >";
-		cin>>sendbuf;//缓冲区
-		if(strcmp(sendbuf,"quit")==0)//相等表示服务器请求退出 
-		{
-			break;
-		}
-		send(sockConn,sendbuf,strlen(sendbuf)+1,0);//发送服务器  +1是为了字符串的/0  3个参数分别是要发送的套接字，缓冲，长度 
-		
-		recv(sockConn,recvbuf,256,0);//接受
-		cout<<"Cli:>"<<recvbuf<<endl;
-		 
-	}
-	
-	closesocket(sockSer);//	
-	WSACleanup();//清除版本信息
-	
-	return 0;
+    const int BUF_SIZE = 64;
+    WSADATA         wsd;            //WSADATA变量
+    SOCKET          sServer;        //服务器套接字
+    SOCKET          sClient;        //客户端套接字
+    SOCKADDR_IN     addrServ;;      //服务器地址
+    char            buf[BUF_SIZE];  //接收数据缓冲区
+    char            sendBuf[BUF_SIZE];//返回给客户端得数据
+    int             retVal;         //返回值
+								   //初始化套结字动态库
+    if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
+    {
+        cout << "WSAStartup failed!" << endl;
+        return 1;
+    }
+
+    //创建套接字
+    sServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == sServer)
+    {
+        cout << "socket failed!" << endl;
+        WSACleanup();//释放套接字资源;
+        return  -1;
+    }
+
+    //服务器套接字地址 
+    addrServ.sin_family = AF_INET;
+    addrServ.sin_port = htons(4999);
+    addrServ.sin_addr.s_addr = INADDR_ANY;
+    //绑定套接字
+    
+    retVal = bind(sServer, (LPSOCKADDR)&addrServ, sizeof(SOCKADDR_IN));
+    if (SOCKET_ERROR == retVal)
+    {
+        cout << "bind failed!" << endl;
+        closesocket(sServer);   //关闭套接字
+        WSACleanup();           //释放套接字资源;
+        return -1;
+    }
+
+    //开始监听 
+    
+    retVal = listen(sServer, 1);
+    if (SOCKET_ERROR == retVal)
+    {
+        cout << "listen failed!" << endl;
+        closesocket(sServer);   //关闭套接字
+        WSACleanup();           //释放套接字资源;
+        return -1;
+    }
+
+    //接受客户端请求
+    sockaddr_in addrClient;
+    int addrClientlen = sizeof(addrClient);
+    sClient = accept(sServer, (sockaddr FAR*)&addrClient, &addrClientlen);
+    if (INVALID_SOCKET == sClient)
+    {
+        cout << "accept failed!" << endl;
+        closesocket(sServer);   //关闭套接字
+        WSACleanup();           //释放套接字资源;
+        return -1;
+    }
+
+    while (true)
+    {
+        //接收客户端数据
+        ZeroMemory(buf, BUF_SIZE);
+        retVal = recv(sClient, buf, BUF_SIZE, 0);
+        if (SOCKET_ERROR == retVal)
+        {
+            cout << "recv failed!" << endl;
+            closesocket(sServer);   //关闭套接字
+            closesocket(sClient);   //关闭套接字     
+            WSACleanup();           //释放套接字资源;
+            return -1;
+        }
+        if (buf[0] == '0')
+            break;
+        cout << "客户端发送的数据: " << buf << endl;
+
+        cout << "向客户端发送数据: ";
+        cin >> sendBuf;
+
+        send(sClient, sendBuf, strlen(sendBuf), 0);
+    }
+
+    //退出
+    closesocket(sServer);   //关闭套接字
+    closesocket(sClient);   //关闭套接字
+    WSACleanup();           //释放套接字资源;
+
+    return 0;
 }
  
